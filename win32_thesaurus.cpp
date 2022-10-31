f953c92edfb564bf21b52628ebf796310491e42b
Adam Kowalewski <ram.techen@gmail.com>
2022-10-31 17:32:01 +0100

    Get out with this shitty toolbar

--- win32_thesaurus.cpp
+++ win32_thesaurus.cpp
@@ -19,6 +19,14 @@ DebugPrintf(const char* Format, ...)
     OutputDebugStringA(Buffer);
 }
 
+static bool
+FileExists(LPCTSTR szPath)
+{
+    DWORD dwAttrib = GetFileAttributes(szPath);
+    return(dwAttrib != INVALID_FILE_ATTRIBUTES &&
+           !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
 #define ListBox_GetCurSel(Handle) SendMessage(Handle, LB_GETCURSEL, 0, 0)
 
 #define StatusBar_SetText(Handle, Index, Text) SendMessage(Handle, SB_SETTEXT, Index, (LPARAM)Text)
@@ -31,14 +39,9 @@ static CThesaurus Thesaurus;
 
 static HWND MainHandle;
 static HWND StatusBarHandle;
-static HWND ToolBarHandle;
 static HWND WordEditHandle;
 static HWND WordsListBoxHandle;
 static HWND SynonymsListBoxHandle;
-
-#define TOOLBAR_IMPORT_ID 0
-#define TOOLBAR_BACK_ID 1
-#define TOOLBAR_FORWARD_ID 2
 
 static void
 ChangeCurrentWord(char* Word)
@@ -74,90 +77,6 @@ WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 
     switch(Message)
     {
-        case WM_NOTIFY:
-        {
-            NMHDR* Header = (NMHDR*) LParam;
-            DebugPrintf("WM_NOTIFY code: %d\n", Header->code);
-            switch(Header->code)
-            {
-                case NM_CLICK:
-                {
-                    if(Header->hwndFrom == ToolBarHandle)
-                    {
-                        NMMOUSE* MouseNotification = (NMMOUSE*) LParam;
-
-                        switch(MouseNotification->dwItemSpec)
-                        {
-                            case TOOLBAR_IMPORT_ID:
-                            {
-                                char FileName[256];
-                                FileName[0] = '\0';
-
-                                OPENFILENAMEA OpenFileName = {0};
-                                OpenFileName.lStructSize = sizeof(OpenFileName);
-                                OpenFileName.hwndOwner = Window;
-                                OpenFileName.hInstance = GetModuleHandleA(0);
-                                OpenFileName.lpstrFilter = "JSON files\0*.json\0";
-                                OpenFileName.lpstrFile = FileName;
-                                OpenFileName.nMaxFile = sizeof(FileName);
-
-                                if(GetOpenFileNameA(&OpenFileName))
-                                {
-                                    CThesaurus NewThesaurus;
-
-                                    StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Loading...");
-                                    StatusBar_SetText(StatusBarHandle, STATUSBAR_MISC_ID, "");
-
-                                    if(ImportThesaurusFromWordNetJsonFile(&NewThesaurus, OpenFileName.lpstrFile))
-                                    {
-                                        StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Data loaded");
-
-                                        ListBox_ResetContent(WordsListBoxHandle);
-
-                                        std::vector<std::string>* AllWords = NewThesaurus.GetAllWords();
-
-                                        char WordsCountText[256];
-                                        snprintf(WordsCountText, sizeof(WordsCountText), "Words count: %d", (int)AllWords->size());
-
-                                        StatusBar_SetText(StatusBarHandle, STATUSBAR_MISC_ID, WordsCountText);
-
-                                        for(auto& Word : *AllWords)
-                                        {
-                                            ListBox_AddString(WordsListBoxHandle, Word.c_str());
-                                        }
-
-                                        delete AllWords;
-
-                                        Thesaurus = std::move(NewThesaurus);
-                                    }
-                                    else
-                                    {
-                                        StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Failed to load");
-
-                                        MessageBoxA(Window, "Failed to import thesaurus", "Error", MB_ICONERROR|MB_OK);
-                                    }
-                                }
-                                else
-                                {
-                                    DebugPrintf("Cancelled\n");
-                                }
-                            } break;
-
-                            case TOOLBAR_BACK_ID:
-                            {
-
-                            } break;
-
-                            case TOOLBAR_FORWARD_ID:
-                            {
-
-                            } break;
-                        }
-                    }
-                } break;
-            }
-        } break;
-
         case WM_COMMAND:
         {
             HWND CommandFrom = (HWND)LParam;
@@ -220,8 +139,6 @@ WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
 INT WINAPI
 WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT CommandShow)
 {
-    // ImportThesaurusFromWordNetJsonFile(&Thesaurus, "file.json");
-
     const char* WindowClassName = "ThesaurusWindowClass";
 
     INITCOMMONCONTROLSEX InitCommonControlsExStruct;
@@ -278,37 +195,6 @@ WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT Comman
     int ControlWidth = (MainWidth - 3 * Margin) / 2;
     int ControlHeight = (int)(10 * (float)(VerticalUnits) / 8);
 
-    const DWORD buttonStyles = 0;
-
-    ToolBarHandle = CreateWindowEx(0, TOOLBARCLASSNAME, NULL,
-                                   WS_CHILD | TBSTYLE_WRAPABLE, 0, 0, 0, 0,
-                                   WindowHandle, NULL, Instance, NULL);
-    Assert(ToolBarHandle);
-
-    TBBUTTON ToolBarButtons[] =
-    {
-        { MAKELONG(HIST_ADDTOFAVORITES,  ImageListID), TOOLBAR_IMPORT_ID, TBSTATE_ENABLED, buttonStyles, {0}, 0, (INT_PTR)"Import"},
-        { 0, 0, 0, BTNS_SEP, {0}, 0, 0},
-        { MAKELONG(HIST_BACK, ImageListID), TOOLBAR_BACK_ID, TBSTATE_ENABLED, buttonStyles, {0}, 0, (INT_PTR)"Back"},
-        { 0, 0, 0, BTNS_SEP, {0}, 0, 0},
-        { MAKELONG(HIST_FORWARD, ImageListID), TOOLBAR_FORWARD_ID, TBSTATE_ENABLED, buttonStyles, {0}, 0, (INT_PTR)"Forward"},
-    };
-
-    HIMAGELIST ImageList = ImageList_Create(bitmapSize, bitmapSize, ILC_COLOR16 | ILC_MASK, ArrayLength(ToolBarButtons), 0);
-
-    SendMessage(ToolBarHandle, TB_SETIMAGELIST,     (WPARAM)ImageListID,                 (LPARAM)ImageList);
-    SendMessage(ToolBarHandle, TB_LOADIMAGES,       (WPARAM)IDB_HIST_SMALL_COLOR,        (LPARAM)HINST_COMMCTRL);
-    SendMessage(ToolBarHandle, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON),            (LPARAM)0);
-    SendMessage(ToolBarHandle, TB_ADDBUTTONS,       (WPARAM)ArrayLength(ToolBarButtons), (LPARAM)&ToolBarButtons);
-    SendMessage(ToolBarHandle, TB_SETINDENT,        (WPARAM)Margin,                          (LPARAM)0);
-    SendMessage(ToolBarHandle, TB_AUTOSIZE,         (WPARAM)0,                           (LPARAM)0);
-
-    ShowWindow(ToolBarHandle,  TRUE);
-
-    RECT ToolBarRect;
-    Assert(GetWindowRect(ToolBarHandle, &ToolBarRect));
-    int ToolBarHeight = ToolBarRect.bottom - ToolBarRect.top;
-
     StatusBarHandle = CreateWindow(STATUSCLASSNAME, "",
                                    WS_CHILD | WS_VISIBLE,
                                    0, 0, 0, 0,
@@ -316,24 +202,22 @@ WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT Comman
     Assert(StatusBarHandle);
     int StatusBarPartsCoordinates[] = { MainWidth/2, -1 };
     Assert(StatusBar_SetParts(StatusBarHandle, ArrayLength(StatusBarPartsCoordinates), StatusBarPartsCoordinates));
-    Assert(StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Ready"));
-    Assert(StatusBar_SetText(StatusBarHandle, STATUSBAR_MISC_ID, "Press IMPORT button to load data"));
 
     RECT StatusBarRect;
     Assert(GetWindowRect(StatusBarHandle, &StatusBarRect));
     int StatusBarHeight = StatusBarRect.bottom - StatusBarRect.top;
 
-    int RemainingHeight = MainHeight - StatusBarHeight - ToolBarHeight;
+    int RemainingHeight = MainHeight - StatusBarHeight;
 
-    int Top = ToolBarHeight + Margin;
+    int Top = Margin;
 
     {
         int Y = Top;
 
         WordEditHandle = CreateWindow(WC_EDIT, "",
-                                          WS_CHILD | WS_VISIBLE | WS_BORDER,
-                                          Margin, Y, ControlWidth, ControlHeight,
-                                          WindowHandle, NULL, Instance, NULL);
+                                      WS_CHILD | WS_VISIBLE | WS_BORDER,
+                                      Margin, Y, ControlWidth, ControlHeight,
+                                      WindowHandle, NULL, Instance, NULL);
         Assert(WordEditHandle);
 
         Y += ControlHeight + Margin;
@@ -351,6 +235,59 @@ WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT Comman
                                              ControlWidth + 2*Margin, Top, ControlWidth, RemainingHeight - 2*Margin,
                                              WindowHandle, NULL, Instance, NULL);
         Assert(WordsListBoxHandle);
+    }
+
+    char FileName[256] = "file.json";
+
+    if(!FileExists(FileName))
+    {
+        int UserChoice = MessageBoxA(WindowHandle, "Synonyms database file does not exist.\nWould you like to select other file?", "Thesaurus", MB_YESNO|MB_ICONQUESTION);
+        if(UserChoice == IDYES)
+        {
+            FileName[0] = '\0';
+
+            OPENFILENAMEA OpenFileName = {0};
+            OpenFileName.lStructSize = sizeof(OpenFileName);
+            OpenFileName.hwndOwner = WindowHandle;
+            OpenFileName.hInstance = GetModuleHandleA(0);
+            OpenFileName.lpstrFilter = "JSON files\0*.json\0All files\0*.*\0";
+            OpenFileName.lpstrFile = FileName;
+            OpenFileName.nMaxFile = sizeof(FileName);
+
+            GetOpenFileNameA(&OpenFileName);
+        }
+    }
+
+    StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Loading...");
+    StatusBar_SetText(StatusBarHandle, STATUSBAR_MISC_ID, "");
+
+    if(ImportThesaurusFromWordNetJsonFile(&Thesaurus, FileName))
+    {
+        StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Data loaded");
+
+        ListBox_ResetContent(WordsListBoxHandle);
+
+        std::vector<std::string>* AllWords = Thesaurus.GetAllWords();
+
+        char WordsCountText[256];
+        snprintf(WordsCountText, sizeof(WordsCountText), "Words count: %d", (int)AllWords->size());
+
+        StatusBar_SetText(StatusBarHandle, STATUSBAR_MISC_ID, WordsCountText);
+
+        for(auto& Word : *AllWords)
+        {
+            ListBox_AddString(WordsListBoxHandle, Word.c_str());
+        }
+
+        delete AllWords;
+
+        Assert(StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Data loaded"));
+    }
+    else
+    {
+        StatusBar_SetText(StatusBarHandle, STATUSBAR_OVERALL_ID, "Failed to load data");
+
+        MessageBoxA(WindowHandle, "Failed to import thesaurus database", "Error", MB_ICONERROR|MB_OK);
     }
 
     MSG Message;
